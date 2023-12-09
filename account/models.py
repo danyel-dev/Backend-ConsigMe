f44f28e6b7083deacac55800e9ede08a82b14f0f
danyel-dev <dannykill4221x@gmail.com>
2023-12-09 11:18:16 -0300

    field cep added to addredd model

--- account/models.py
+++ account/models.py
@@ -1,27 +1,19 @@
-from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import User
 from django.db import models
 from datetime import datetime
 
 
-
-
 class address(models.Model):
+  cep = models.CharField(max_length=8)
   state = models.CharField(max_length=20)
   city = models.CharField(max_length=50)
   district = models.CharField(max_length=50)
   street = models.CharField(max_length=50)  
-  street = models.CharField(max_length=50)
   complement = models.CharField(max_length=100)
-
 
  
   def __str__(self):
       return self.city
-
-
 
 
 class profile(models.Model):
@@ -39,5 +31,7 @@ class profile(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
  
+ 
   def __str__(self):
       return self.store_name
+  
