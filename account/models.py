0f1207f946d2e7e2516e0fc810bc666447ef4c4c
danyel-dev <dannykill4221x@gmail.com>
2023-12-11 12:39:51 -0300

    a

--- account/models.py
+++ account/models.py
@@ -3,19 +3,6 @@ from django.db import models
 from datetime import datetime
 
 
-class address(models.Model):
-  cep = models.CharField(max_length=8)
-  state = models.CharField(max_length=20)
-  city = models.CharField(max_length=50)
-  district = models.CharField(max_length=50)
-  street = models.CharField(max_length=50)  
-  complement = models.CharField(max_length=100)
-
- 
-  def __str__(self):
-      return self.city
-
-
 class profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.PROTECT)
   store_name = models.CharField(max_length=50)
@@ -25,9 +12,15 @@ class profile(models.Model):
   birth_date = models.DateField()
   bio = models.TextField(blank=True, null=True)
   image = models.ImageField(blank=True, null=True, upload_to=f'profileImages/{datetime.now().strftime("%d_%m_%Y")}')
-  address = models.OneToOneField(address, on_delete=models.CASCADE)
+  cep = models.CharField(max_length=8)
+  state = models.CharField(max_length=20)
+  city = models.CharField(max_length=50)
+  district = models.CharField(max_length=50)
+  street = models.CharField(max_length=50)  
+  house_number = models.IntegerField()
+  complement = models.CharField(max_length=100)
   quantity_products_sold = models.IntegerField(default=0, null=True, blank=True)  
-  is_reseller = models.BooleanField(default=False)
+  is_reseller = models.BooleanField(default=False, null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
  
